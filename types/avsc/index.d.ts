3c8f5660e8386fe32b7a919d0415c1b4aaf58736
jewbre <jewbre18@gmail.com>
2018-03-09 09:17:39 +0100

    inital commit

---
+++ types/avsc/index.d.ts
@@ -0,0 +1,5 @@
+// Type definitions for avsc 5.2
+// Project: https://github.com/mtth/avsc
+// Definitions by: My Self <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
